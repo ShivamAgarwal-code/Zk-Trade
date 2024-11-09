541c79229ba73d7c1399b4b9e5e7a673b6a59aa6
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2024-11-09 14:38:39 +0530

    Update tradezk.js

--- contracts/test/tradezk.js
+++ contracts/test/tradezk.js
@@ -6,9 +6,7 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("Tradezk", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
+ 
   async function accounts() {
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
