1cd54962cc565e272c269b29d38001f3d1d8bb70
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2024-11-09 14:37:15 +0530

    Update IERC20.sol

--- contracts/contracts/IERC20.sol
+++ contracts/contracts/IERC20.sol
@@ -7,12 +7,7 @@ pragma solidity ^0.8.0;
  * @dev Interface of the ERC-20 standard as defined in the ERC.
  */
 interface IERC20 {
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
+    
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     /**
